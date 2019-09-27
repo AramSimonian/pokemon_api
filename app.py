2097d6f52cd9d10d966f376fb267548db0a3ec73
Aram Simonian <aram.contacts@gmail.com>
2019-09-28 00:02:03 +0100

    Refactored get_pokemon_by_name so processing is done elsewhere

--- app.py
+++ app.py
@@ -2,13 +2,39 @@ from flask import Flask, jsonify
 import requests
 import json
 
+language_code = "en"
+
 app = Flask(__name__)
+
+def extract_ft_for_language(flavor_text_entries, language_code):
+    ft_for_language = []
+    for fte in flavor_text_entries:
+        # The language code is stored within a sub-dictionary
+        if fte["language"]["name"] == language_code:
+            # \n will break the API call
+            ft_for_language.append(fte["flavor_text"].replace('\n', ' '))
+
+    # This will contain a list of descriptions for a character
+    # from all the different game versions
+    return ft_for_language
 
 
 @app.route('/pokemon/<string:pokemon_name>')
 def get_pokemon_by_name(pokemon_name):
-    response = requests.get('https://pokeapi.co/api/v2/pokemon-species/%s' % pokemon_name)
-    return jsonify(response.json())
+    try:
+        response = requests.get('https://pokeapi.co/api/v2/pokemon-species/%s' % pokemon_name)
 
+        fte_all = response.json()["flavor_text_entries"]
+        ft_for_language_all = extract_ft_for_language(fte_all, language_code)
+
+        # Just choose the first description
+        # until requirements change
+        pokemon_description = ft_for_language_all[0]
+
+        
+
+        return jsonify(pokemon_description)
+    except:
+        return 'Sorry, that Pokemon name was not found'
 
 app.run(port=5000)
---
+++ tests/test_pokemon_api.py
@@ -0,0 +1,5 @@
+pokemon_api = 'https://pokeapi.co/api/v2/pokemon-species/'
+pokemon_name = 'charizard'
+
+# def test_valid_pokemon_call():
+
